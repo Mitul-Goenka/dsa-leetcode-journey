bdb074789d7049a18c395e55618201ba86431470
Mitul Goenka <mitulgoenka09@gmail.com>
2026-06-29 16:20:21 +0530

    Time: 3 ms (49.69%), Space: 45.9 MB (98.77%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character , Integer> map1 = new HashMap<>();
-        HashMap<Character , Integer> map2 = new HashMap<>();
-        for(int i=0;i<ransomNote.length();i++){
-            char c = ransomNote.charAt(i);
-            map1.put(c , map1.getOrDefault(c,0)+1);
-        }
+        int[] freq = new int[26];
         for(int i=0;i<magazine.length();i++){
-            char c = magazine.charAt(i);
-            map2.put(c , map2.getOrDefault(c,0)+1);
+            freq[magazine.charAt(i)-'a']++;
         }
         for(int i=0;i<ransomNote.length();i++){
-            if(!map2.containsKey(ransomNote.charAt(i))  || map1.get(ransomNote.charAt(i))>map2.get(ransomNote.charAt(i))){
+            freq[ransomNote.charAt(i)-'a']--;
+            if(freq[ransomNote.charAt(i)-'a'] < 0){
                 return false;
             }
+        }
+        for(int i=0;i<ransomNote.length();i++){
+           
         }
         return true;
 
