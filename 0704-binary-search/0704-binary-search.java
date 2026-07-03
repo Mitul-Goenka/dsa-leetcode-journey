498072ceb3eafc451eb1685d7186246a840b9436
Mitul Goenka <mitulgoenka09@gmail.com>
2026-07-03 13:00:25 +0530

    Time: 113 ms (0.47%), Space: 48.5 MB (13.91%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while(low<=high){
+            int i = (low+high)/2;
+
+            if(nums[i]==target){
+                return i;
+            }
+            else if(nums[i]<target){
+                low = i+1;
+            }
+            else{
+                high = i-1;
+            }
+        }
+        return -1;
+    }
+}
