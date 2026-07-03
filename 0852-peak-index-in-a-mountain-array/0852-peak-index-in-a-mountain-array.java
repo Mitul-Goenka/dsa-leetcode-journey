6774f71482f6146299fc972f999206b723337cfe
Mitul Goenka <mitulgoenka09@gmail.com>
2026-07-03 17:28:48 +0530

    Time: 0 ms (100%), Space: 80.4 MB (39.23%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,13 +1,18 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        Stack<Integer> st = new Stack<>();
-        st.push(0);
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]>arr[st.peek()]){
-                st.push(i);
+        int low = 0;
+        int high = arr.length - 1;
+        while(low<high){
+            int mid = low + (high-low)/2;
+
+            if(arr[mid]<arr[mid+1]){
+                low = mid+1;
             }
+            else{
+                high = mid;
+            }
+            
         }
-        return st.peek();
-        
+        return low;
     }
 }
