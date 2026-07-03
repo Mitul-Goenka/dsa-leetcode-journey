54fe2d43d73803fb48640031ea0445eb8244bca8
Mitul Goenka <mitulgoenka09@gmail.com>
2026-07-03 16:07:13 +0530

    Time: 0 ms (100%), Space: 48.1 MB (50.41%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,7 +2,7 @@ class Solution {
     public int findLeft(int[] nums , int target){
         int low = 0;
         int high = nums.length - 1;
-        int ans = 0;
+        int ans = -1;
         while(low<=high){
             int mid = low + (high-low)/2;
             if(nums[mid]==target){
@@ -21,7 +21,7 @@ class Solution {
     public int findRight(int[] nums , int target){
         int low = 0;
         int high = nums.length - 1;
-        int ans = 0;
+        int ans = -1;
         while(low<=high){
             int mid = low + (high-low)/2;
             if(nums[mid]==target){
@@ -38,26 +38,8 @@ class Solution {
         return ans;
     }
     public int[] searchRange(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
-        int[] res = new int[nums.length];
-        int ans = 0;
-        while(low<=high){
-            int i = low + (high-low)/2;
-
-             if(nums[i]>target){
-                high = i-1;
-            }
-            else if(nums[i]<target){
-                low = i+1;
-            }
-            else{
-                int first = findLeft(nums, target);
-                int last = findRight(nums , target);
-                return new int[]{first , last};
-            }
-
-        }
-        return new int[]{-1,-1};
+        int first = findLeft(nums, target);
+        int last = findRight(nums , target);
+        return new int[]{first , last};  
     }
 }
