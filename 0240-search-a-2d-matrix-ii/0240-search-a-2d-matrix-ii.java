941ed8400419ce6bc9ec7e75c587758c637257ca
Mitul Goenka <mitulgoenka09@gmail.com>
2026-07-08 12:26:52 +0530

    Time: 3 ms (89.23%), Space: 48 MB (83.44%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -3,11 +3,11 @@ class Solution {
         if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
             return false;
         }
-        int n = matrix.length;
-        int m = matrix[0].length;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
         int row = 0;
-        int col = m-1;
-        while(row<n && col>=0){
+        int col = cols-1;
+        while(row<rows && col>=0){
             if(matrix[row][col]==target){
                 return true;
             }
