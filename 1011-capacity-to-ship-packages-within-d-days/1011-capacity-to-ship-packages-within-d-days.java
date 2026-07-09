26f3db0d64728bcdb8a524367ff074727573ce3c
Mitul Goenka <mitulgoenka09@gmail.com>
2026-07-09 13:39:38 +0530

    Time: 11 ms (68.32%), Space: 50.1 MB (75.75%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -5,19 +5,15 @@ class Solution {
         for(int i=0;i<weights.length;i++){
             weight += weights[i];
             if(weight > guess){
-                weight = weight-weights[i];
                 count++;
                 weight = weights[i];
                 }      
-            else{
-                continue;
-            }
         }
         return count<=days;
     }
     public int shipWithinDays(int[] weights, int days) {
         int high = 0;
-        int low = Integer.MIN_VALUE;
+        int low = 0;
         for(int i=0;i<weights.length;i++){
             low = Math.max(low , weights[i]);
             high += weights[i]; 
