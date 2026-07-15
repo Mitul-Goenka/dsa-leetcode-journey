7191d9684017e73e28a64cb858f2d7e0db3efd48
Mitul Goenka <mitulgoenka09@gmail.com>
2026-07-15 16:35:15 +0530

    Time: 6 ms (99.59%), Space: 47.7 MB (87.57%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,32 +1,32 @@
 class Solution {
-    public long fun(int[] piles , int h , int speed){
-        long time = 0;
+    public long findSpeed(int[] piles, int h , int speed){
+        long hours = 0;
+        
         for(int i=0;i<piles.length;i++){
-            time += piles[i]/speed;
-            if(piles[i]%speed != 0){
-                time++;
-            }
+            hours += (piles[i] + speed - 1)/speed;
         }
-        return time;
+        return hours;
     }
     public int minEatingSpeed(int[] piles, int h) {
         int low = 1;
         int high = 0;
+        
         for(int i=0;i<piles.length;i++){
             high = Math.max(piles[i] , high);
         }
-        int res = -1;
-        while(low <= high){
-            int guess = low+(high-low)/2;
-            long hour = fun(piles , h , guess);
-            if(hour<=h){
-                res = guess;
-                high = guess-1;
+        int ans = high;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            long hours = findSpeed(piles , h , mid);
+
+            if(hours<=h){
+                ans = mid;
+                high = mid-1;
             }
             else{
-                 low = guess+1;
+                low = mid+1;
             }
         }
-        return res;
+        return ans;
     }
 }
