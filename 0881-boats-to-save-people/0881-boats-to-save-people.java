273e33dacfee8b573725a44cb3e7a7f9a87195fa
Mitul Goenka <mitulgoenka09@gmail.com>
2026-07-19 22:34:02 +0530

    Time: 19 ms (98.51%), Space: 56.4 MB (56.74%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -6,16 +6,11 @@ class Solution {
         int right = people.length - 1;
         while(left<=right){
             int sum = people[left]+people[right];
-
             if(sum<=limit){
-                boats++;
                 left++;
-                right--;
             }
-            else if(sum>limit && people[right]<=limit){
                 boats++;
                 right--;
-            }
         }
         return boats;
     }
