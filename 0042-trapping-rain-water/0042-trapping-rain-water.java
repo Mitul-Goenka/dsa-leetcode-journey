e8960e403f7f286edb58227c419c40723508c1e4
Mitul Goenka <mitulgoenka09@gmail.com>
2026-07-20 00:06:42 +0530

    Time: 1 ms (60.41%), Space: 47.6 MB (69.37%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -5,10 +5,10 @@ class Solution {
         int leftMax = 0;
         int rightMax = 0;
         int count = 0;
-        while(left<right){
+        while(left<=right){
             leftMax = Math.max(height[left] , leftMax);
             rightMax = Math.max(height[right] , rightMax);
-            if(height[left] < height[right]){
+            if(leftMax < rightMax){
                 count += leftMax - height[left];
                 left++;
             }
