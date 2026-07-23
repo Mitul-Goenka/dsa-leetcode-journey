69ee24968cac99413144773b482eca2cbaf1964d
Mitul Goenka <mitulgoenka09@gmail.com>
2026-07-23 22:37:00 +0530

    Time: 2 ms (92.13%), Space: 47.8 MB (38.33%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums.length<=1){
+            return;
+        }
+        int pos = 0;
+        int j = 0;
+        int zero = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[pos] = nums[i];
+                pos++;
+            }
+            else {
+                zero++;
+            }
+        }
+        for(int i=nums.length-zero;i<nums.length;i++){
+            nums[i] = 0;
+        }
+    }
+}
