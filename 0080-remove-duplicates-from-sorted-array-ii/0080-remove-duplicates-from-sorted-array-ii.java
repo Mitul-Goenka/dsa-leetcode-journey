d2c4d146e2aa125acec73b1f153fc551db16bf9e
Mitul Goenka <mitulgoenka09@gmail.com>
2026-07-23 19:24:24 +0530

    Time: 0 ms (100%), Space: 48.6 MB (84.36%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -2,19 +2,16 @@ class Solution {
     public int removeDuplicates(int[] nums) {
         int i=0;
         int j=1;
-        int count=1;
-        int a=0;
         while(j<nums.length){
             if(i>=1 && nums[j]==nums[i] && nums[j]==nums[i-1]){
                 j++;
             }
             else{
                 nums[i+1] = nums[j];
-                count++;
                 j++;
                 i++;
             }
         }
-        return count;
+        return i+1;
     }
 }
