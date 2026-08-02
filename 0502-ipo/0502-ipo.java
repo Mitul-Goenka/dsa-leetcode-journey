2480243f728524a56e1e61196c82601498fe2321
Mitul Goenka <mitulgoenka09@gmail.com>
2026-08-02 17:11:02 +0530

    Time: 103 ms (68.49%), Space: 137.1 MB (17.88%) - LeetHub

---
+++ 0502-ipo/0502-ipo.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        Pair[] arr = new Pair[profits.length];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = new Pair(capital[i] , profits[i]);
+        }
+        Arrays.sort(arr , (a,b)-> Integer.compare(a.cap,b.cap));
+        PriorityQueue<Integer> pq2 = new PriorityQueue<>(Collections.reverseOrder());
+        int idx = 0;
+        while(k>0){
+        while(idx < profits.length){
+            if(arr[idx].cap > w){
+                break;
+            }
+            pq2.add(arr[idx].prof);
+            idx++;
+        }
+        if(pq2.isEmpty()){
+            return w;
+        }
+        w += pq2.poll();
+        k--;
+        }
+    return w;
+    }
+}
+class Pair{
+    int cap;
+    int prof;
+    Pair(int cap , int prof){
+        this.cap = cap;
+        this.prof = prof;
+    }
+}
