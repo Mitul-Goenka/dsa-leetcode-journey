b83c7f867cd5b2ad39b1d244bfd066fca1445a26
Mitul Goenka <mitulgoenka09@gmail.com>
2026-08-08 02:33:10 +0530

    Time: 66 ms (5.27%), Space: 57.3 MB (62.11%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int scheduleCourse(int[][] courses) {
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+            (a, b) -> {
+                if (a.lastDay != b.lastDay) {
+                    return a.lastDay - b.lastDay;
+                }
+                return a.totalDuration - b.totalDuration;
+            }
+        );
+        PriorityQueue<Integer> pq2 = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < courses.length; i++) {
+            pq.add(new Pair(courses[i][1], courses[i][0]));
+        }
+        int td = 0;
+        int count = 0;
+        while (!pq.isEmpty()) {
+            Pair P = pq.poll();
+            pq2.add(P.totalDuration);
+            td += P.totalDuration;
+            int ld = P.lastDay;
+            if (td > ld) {
+                td = td - pq2.poll();
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+class Pair {
+    int lastDay;
+    int totalDuration;
+    Pair(int lastDay, int totalDuration) {
+        this.lastDay = lastDay;
+        this.totalDuration = totalDuration;
+    }
+}
